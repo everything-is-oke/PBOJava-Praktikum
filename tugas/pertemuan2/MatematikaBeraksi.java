7c5568d45f96e0e08495c7ad1e9c589c68d5dca0
everything-is-oke <2210631170110@student.unsika.ac.id>
2023-10-18 16:34:50 +0700

    [refactor] kirim nilai dengan parameter secara langsung

--- tugas/pertemuan2/MatematikaBeraksi.java
+++ tugas/pertemuan2/MatematikaBeraksi.java
@@ -2,17 +2,14 @@ public class MatematikaBeraksi {
     public static void main(String[] args) {
         Matematika matematika = new Matematika();
 
-        // deklarasi variabel a dan b
-        int a = 20, b = 10;
+        int hasilPertambahan = matematika.pertambahan(20, 10);
+        int hasilPengurangan = matematika.pengurangan(10, 5);
+        int hasilPerkalian = matematika.perkalian(10, 3);
+        double hasilPembagian = matematika.pembagian(21, 2);
 
-        int hasilPertambahan = matematika.pertambahan(a, b);
-        int hasilPengurangan = matematika.pengurangan(a, b);
-        int hasilPerkalian = matematika.perkalian(a, b);
-        double hasilPembagian = matematika.pembagian(a, b);
-
-        System.out.println("Pertambahan: " + a + " + " + b + " = " + hasilPertambahan);
-        System.out.println("Pengurangan: " + a + " - " + b + " = " + hasilPengurangan);
-        System.out.println("Perkalian: " + a + " * " + b + " = " + hasilPerkalian);
-        System.out.println("Pembagian: " + a + " / " + b + " = " + hasilPembagian);
+        System.out.println("Pertambahan: " + 20 + " + " + 10 + " = " + hasilPertambahan);
+        System.out.println("Pengurangan: " + 10 + " - " + 5 + " = " + hasilPengurangan);
+        System.out.println("Perkalian: " + 10 + " * " + 3 + " = " + hasilPerkalian);
+        System.out.println("Pembagian: " + 21 + " / " + 2 + " = " + hasilPembagian);
     }
 }
